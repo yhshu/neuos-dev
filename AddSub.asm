c4f5ef6651e9f3199df5f165dcee4d6d1b810896
Yiheng Shu <lzw429@qq.com>
2017-04-17 18:36:59 +0800

    init

---
+++ AddSub.asm
@@ -0,0 +1,14 @@
+TITLE Add and Substract 		(AddSub.asm)
+;This program adds and substracts 32-bit integers.
+
+INCLUDE Irvine32.inc
+.code
+main PROC
+	mov eax,10000h				;EAX=10000h
+	add eax,40000h				;EAX=50000h
+	sub eax,20000h				;EAX=30000h
+	call DumpRegs				;display registers
+
+	exit
+main ENDP
+END main
